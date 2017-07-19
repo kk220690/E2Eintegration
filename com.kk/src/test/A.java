efc722dc7cd73ca85e890601aa69324d32b53fee
kk <kk@kk-PC>
2017-07-19 19:59:53 +0530

    created automation project

---
+++ com.kk/src/test/A.java
@@ -0,0 +1,19 @@
+package test;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class A
+{
+	@Test
+	public void tc_01()
+	{
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\kk\\Desktop\\browser_jar\\chromedriver.exe");
+		WebDriver d=new ChromeDriver();
+		d.get("http://www.facebook.com");
+	}
+
+}
